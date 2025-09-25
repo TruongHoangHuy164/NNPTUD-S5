0334f9595880ee8028a8fca0c63c6ab3f802dd44
Trương Hoàng Huy <truonghoanghuy164@gmail.com>
2025-09-25 11:22:05 +0700

    Done 25Sep_S5_Trương Hoàng Huy

--- schemas/product.js
+++ schemas/product.js
@@ -15,7 +15,8 @@ let schema = new mongoose.Schema({
         default: "good product"
     },
     category:{
-        type:String,
+        type: Schema.Types.ObjectId,
+        ref: 'category',
         required: true
     }
     ,
